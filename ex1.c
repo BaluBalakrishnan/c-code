c88a24b803bdcd547a42d7c3c1257cd040dcccf1
gaga <gangadharan@linumiz.com>
2022-03-25 09:31:46 +0530

    exercise 1

---
+++ ex1.c
@@ -0,0 +1,9 @@
+//#include<stdio.h>:without header file using in this program..
+//main():without returntype using in this main function..
+
+main()
+{
+	printf("hello,world");
+}
+
+//output: its comes to warning message.*implicit declaration*
