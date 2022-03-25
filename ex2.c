89a107fa226a85294fe29d373769c36c12e6e8fc
gaga <gangadharan@linumiz.com>
2022-03-25 15:05:57 +0530

    exercise2

---
+++ ex2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello,\nworld
+			");
+}
+//Now using ( \c ) in this program..
+//output: Unknown escape sequence..print the output is "hello,Cworld"C character is also print.. 
