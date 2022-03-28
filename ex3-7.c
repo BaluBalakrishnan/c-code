f0e5a9e716bc7b2b3a59db2f0eb070ff8dc681ba
GANGA-THARAN-M <gangadharan@linumiz.com>
2022-03-28 17:40:25 +0530

    basic

---
+++ ex3-7.c
@@ -0,0 +1,19 @@
+/* histogram freq of input*/
+include<stdio.h>
+int main()
+{
+        int c;
+        while((c=getchar())!=EOF)
+        {
+                if( c==' '||c=='\n'||c=='\t')
+                {
+			++i;
+                        printf("escape sequence=%d\n",i );
+                         putchar('\n');
+                }
+                        else
+                                putchar('*');
+                        }
+        return 0;
+}
+
