c7f5f051f214e5b6e96682cc6c977131d8f960a6
GANGA-THARAN-M <gangadharan@linumiz.com>
2022-04-01 12:09:54 +0530

    swaping

---
+++ ex41.c
@@ -0,0 +1,20 @@
+/*swaping using macros*/
+
+#include<stdio.h>
+
+#define swap(t,x,y) { t _z; \
+             _z = x;\
+              x = y;\
+              y = _z; }
+
+int main(void)
+{
+    char x,y;
+    x=rand();
+    y=rand();
+    
+    printf("x= %d \t y= %d\n",x,y);
+    swap(int,x,y);
+    printf("x=%d \t y=%d\n",x,y);
+}
+
